6c51c6bee15f187f84aaacba8ba66246f32280f9
undefined <shongs27@naver.com>
2022-05-15 02:47:04 +0900

    [Chore] readme.md 작성

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 export async function fetchSearchField(searchField, pages) {
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchField}&page=${pages}`,
+    `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchField}&page=${pages}`,
   )
   const data = await response.json()
   return data
@@ -8,7 +8,7 @@ export async function fetchSearchField(searchField, pages) {
 
 export async function fetchSearchCategory(searchField, type) {
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchField}&type=${type}`,
+    `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchField}&type=${type}`,
   )
   const data = await response.json()
   return data
