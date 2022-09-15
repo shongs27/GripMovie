67622ecacd11a1c276a53d92a5a64a547e56abd1
undefined <shongs27@naver.com>
2022-09-15 18:24:05 +0900

    [Fix] movieList를 favoritePage로 로직 옮김

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,16 +1,45 @@
 import { useSelector } from 'react-redux';
 
+import { useEffect, useRef } from 'react';
 import styles from './favoritePage.module.scss';
 
-import MovieList from '../components/MovieList';
+import MovieItem from '../components/MovieItem';
+import FavoritesModal from '../components/FavoritesModal';
+import useDragDrop from '../utils/useDragDrop';
 
 export default function FavoritesPage() {
   const favoriteMovies = useSelector((state) => state.favoriteMovies);
+  const selectedMovie = useSelector((state) => state.selectedMovie);
+  const categoryCount = useSelector((state) => state.categoryCount);
+
+  const { dragStart, dragOver, dragDrop } = useDragDrop();
+
+  const listDOM = useRef();
+
+  useEffect(() => {
+    const dom = listDOM.current;
+    if (dom) dom.scrollTo(0, 0);
+  }, [categoryCount]);
 
   return (
     <>
       <div className={styles.title}>즐겨찾기 페이지</div>
-      <MovieList type="favorite" movies={favoriteMovies} />
+      <ul ref={listDOM} className={styles.listContainer}>
+        {favoriteMovies.map((movie, i) => (
+          <div
+            key={movie.imdbID}
+            draggable
+            onDragOver={dragOver}
+            onDragStart={dragStart}
+            onDrop={dragDrop}
+            id={i}
+          >
+            <MovieItem movie={movie} />
+          </div>
+        ))}
+
+        {selectedMovie && <FavoritesModal selectedMovie={selectedMovie} />}
+      </ul>
     </>
   );
 }
